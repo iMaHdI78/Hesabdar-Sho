0956aa4671642e014fbfd72fe776ff01bea117f3
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-04-27 01:17:28 +0330

    Adding information to models and working with models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Article(models.Model):
+    STATUS_CHOICES= (
+        ('d','Draft'),
+        ('p','Publish'),
+        
+    )
+    title = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length=100 , unique=True)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to="images")
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add = True) #برای زمانی که مقاله کی ایجاد شده چه منتشر شود چه نشود
+    updated = models.DateTimeField(auto_now = True) #این برای زمانی هستش که نمایش می دهد کی ملاقه عوض شده
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES)
