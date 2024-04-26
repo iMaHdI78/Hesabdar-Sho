e6ce46d771423b98960c877f01630e0d0fedbd83
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-04-26 14:11:49 +0330

    Add render and templates

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello word")
+    context={
+        
+    }
+    return render(request,"blog/home.html", context)
