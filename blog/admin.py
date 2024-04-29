ec4a6a39f90d7b956af31b1de2f7a3c1ab9e38d4
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-04-29 15:29:21 +0330

    Configuring the Django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,31 @@
 from django.contrib import admin
 from .models import Article
+
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'publish',
+        'status',
+        
+    )
+    
+    list_filter = (
+        'publish',
+        'status',
+        
+    )
+
+    search_fields = (
+        'title',
+        'description',
+    )
+    
+    prepopulated_fields = {'slug' : ('title',)} #برای اینکه فیلد اسلاک از فیلد تایتل به صورت خودکار پر شود
+    
+    ordering = [
+        '-status',
+        '-publish',
+    ]
+admin.site.register(Article,ArticleAdmin)
