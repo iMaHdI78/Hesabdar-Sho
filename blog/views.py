9ce2041b0d64d93e8d269e3d9b3a7919d9ebb6b8
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-04-29 16:19:40 +0330

    Setting up the management panel in Django, receiving database information through the model and displaying it in the view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 def home(request):
     context={
-        
+        "articles" : Article.objects.filter(status='p').order_by('-publish')#[:3]
     }
     return render(request,"blog/home.html", context)
