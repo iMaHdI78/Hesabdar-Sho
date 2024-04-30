24432fd494a53b2b9f76d31ae28b5324dd6c4456
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-04-30 22:00:09 +0330

    Adding Farsi language to the panel and fixing the clock bug

--- blog/models.py
+++ blog/models.py
@@ -5,18 +5,22 @@ from django.utils import timezone
 
 class Article(models.Model):
     STATUS_CHOICES= (
-        ('d','Draft'),
-        ('p','Publish'),
+        ('d','پیش نویس'),
+        ('p','منتشر شده'),
         
     )
-    title = models.CharField(max_length = 200)
-    slug = models.SlugField(max_length=100 , unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length = 200 , verbose_name = 'عنوان مقاله')
+    slug = models.SlugField(max_length=100 , unique=True , verbose_name = 'آدرس مقاله')
+    description = models.TextField(verbose_name = 'محتوا')
+    thumbnail = models.ImageField(upload_to="images" , verbose_name = 'تصویر مقاله')
+    publish = models.DateTimeField(default=timezone.now , verbose_name = 'زمان انتشار')
     created = models.DateTimeField(auto_now_add = True) #برای زمانی که مقاله کی ایجاد شده چه منتشر شود چه نشود
-    updated = models.DateTimeField(auto_now = True) #این برای زمانی هستش که نمایش می دهد کی ملاقه عوض شده
-    status = models.CharField(max_length=1,choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now = True)#این برای زمانی هستش که نمایش می دهد کی ملاقه عوض شده
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES , verbose_name = 'وضعیت')
+    
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
     
     #اگر بخوایم یه کلاس بهمون برگرده
     def __str__(self):
