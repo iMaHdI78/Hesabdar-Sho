efa0114ad68269f5ffdddb829ef92125be5db1cd
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-05-03 18:02:43 +0330

    Starting with the category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'position',
+        'title',
+        'slug',
+        'status',
+        
+    )
+    
+    list_filter = (
+        ['status']
+        
+    )
+
+    search_fields = (
+        'title',
+        'slug',
+    )
+    
+    prepopulated_fields = {'slug' : ('title',)} #برای اینکه فیلد اسلاک از فیلد تایتل به صورت خودکار پر شود
+    
+admin.site.register(Category,CategoryAdmin)
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = (
         'title',
         'slug',
         'jpublish',
         'status',
+        'category_to_str',
         
     )
     
@@ -28,4 +52,8 @@ class ArticleAdmin(admin.ModelAdmin):
         '-status',
         '-publish',
     ]
+    
+    def category_to_str(self, obj):
+        return 'Categories'
+        
 admin.site.register(Article,ArticleAdmin)
