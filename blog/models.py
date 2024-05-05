a906bc4e6601e7f15c24005d74a7e59cd32cabe0
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-05-05 23:48:09 +0330

    bug fix

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class ArticleManager(models.Manager):
 # Create your models here.
 
 class Category(models.Model):
-    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL, related_name='choldren', verbose_name="زیر دسته")
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL, related_name='children', verbose_name="زیر دسته")
     title = models.CharField(max_length = 200 , verbose_name = 'عنوان دسته بندی')
     slug = models.SlugField(max_length=100 , unique=True , verbose_name = 'آدرس دسته بندی')
     status = models.BooleanField(default=True , verbose_name='آیا نمایش داده شود؟')
