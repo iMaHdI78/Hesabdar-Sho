900638911e6d5c58745b3aad2b2e5869ad4f965e
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-05-06 00:05:02 +0330

    Nested categories in Django, proper styling

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,10 @@ from extensions.utils import jalali_converter
 class ArticleManager(models.Manager):
     def published(self):
         return self.filter(status = 'p')
+    
+class CategoryManager(models.Manager):
+    def active(self):
+        return self.filter(status = True)
 
 
 # Create your models here.
@@ -25,7 +29,7 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
-
+    objects = CategoryManager()
 
 
 class Article(models.Model):
