09e2b79021129b5b26882c8b4d2ba2fee3dab7f3
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-05-07 15:08:24 +0330

    Improved project codes for better management

--- blog/admin.py
+++ blog/admin.py
@@ -85,7 +85,7 @@ class ArticleAdmin(admin.ModelAdmin):
             ]
 	
 	def category_to_str(self, obj):
-		return ", ".join([category.title for category in obj.category_published()])
+		return ", ".join([category.title for category in obj.category.active()])
 	category_to_str.short_description = 'دسته بندی'   
 			 
 admin.site.register(Article,ArticleAdmin)
