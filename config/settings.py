541ba5d6e5fec96ae2639d52ab58fe3a7d776159
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-05-13 14:29:56 +0330

    Adding custom fields to the User model

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-
+AUTH_USER_MODEL = 'account.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
