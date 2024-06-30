25ce96c01d8fd7b66282fa6edfb70e022eed0297
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-06-30 18:28:27 +0330

    Most visited articles of the month (1) - Introduction to through model

--- blog/models.py
+++ blog/models.py
@@ -67,7 +67,7 @@ class Article(models.Model):
 	is_special = models.BooleanField(default=False , verbose_name='مقاله ی ویژه')
 	status = models.CharField(max_length=1,choices=STATUS_CHOICES , verbose_name = 'وضعیت')
 	comments = GenericRelation(Comment)
-	hits = models.ManyToManyField(IPAddress, blank=True, related_name="hits", verbose_name="بازدید ها")
+	hits = models.ManyToManyField(IPAddress, through = "ArticleHit", blank=True, related_name="hits", verbose_name="بازدید ها")
  
 	
 	class Meta:
@@ -99,3 +99,8 @@ class Article(models.Model):
 			 
  
 	objects = ArticleManager()
+ 
+class ArticleHit(models.Model):
+	article = models.ForeignKey(Article, on_delete=models.CASCADE)
+	ip_address = models.ForeignKey(IPAddress, on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
