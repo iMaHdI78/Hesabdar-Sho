731590dcef36d62bc515b7b8f3e7994040574b64
AmirMahdi AlaeiTabar <117546623+iMaHdI78@users.noreply.github.com>
2024-07-15 13:57:59 +0330

    Most popular articles of query writing month

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render, get_object_or_404
 from account.mixins import AuthorAccessMixin
 # from django.http import HttpResponse, JsonResponse, Http404
 from .models import Article, Category
+from django.db.models import Count, Q
+from datetime import datetime, timedelta
 
 # Create your views here.
 class ArticleList(ListView):
-	queryset = Article.objects.published()
+	last_month = datetime.today() - timedelta(days=30)
+	queryset = Article.objects.published().annotate(
+		count=Count('hits', filter=Q(articlehit__created__gt=last_month))
+	).order_by('-count', '-publish')
 	paginate_by = 5
 
 
@@ -59,4 +64,18 @@ class AuthorList(ListView):
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 		context['author'] = author
+		return context
+
+
+class SearchList(ListView):
+	paginate_by = 1
+	template_name = 'blog/search_list.html'
+
+	def get_queryset(self):
+		search = self.request.GET.get('q')
+		return Article.objects.filter(Q(description__icontains=search) | Q(title__icontains=search))
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['search'] = self.request.GET.get('q')
 		return context
